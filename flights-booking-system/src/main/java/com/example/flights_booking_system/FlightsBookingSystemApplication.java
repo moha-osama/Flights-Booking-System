b0c0428227ff47bdc2e414a71308617c47f64849
moha-osama <zmuhamedosama@gmail.com>
2025-12-27 20:21:03 +0200

    hi joey

---
+++ flights-booking-system/src/main/java/com/example/flights_booking_system/FlightsBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.flights_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsBookingSystemApplication.class, args);
+	}
+
+}
